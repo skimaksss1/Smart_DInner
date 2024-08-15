350a64d5b452b0864a1ff32f709035efbd22f438
skimaksss1 <skimaksss@gmail.com>
2024-08-15 07:10:04 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -53,4 +53,55 @@ function update(percentage) {
     
   });
   
-  
+  function selectClass(event, dropdownId) {
+    event.preventDefault();
+    const selectedClass = event.target.textContent;
+    document.querySelector(`#${dropdownId} .dropbtn`).textContent = selectedClass;
+}
+
+
+function sortTable(n) {
+  const table = document.getElementById("employeeTable");
+  let rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  switching = true;
+  dir = "asc"; 
+
+  while (switching) {
+      switching = false;
+      rows = table.rows;
+
+      for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+
+          if (dir == "asc") {
+              if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                  shouldSwitch = true;
+                  break;
+              }
+          } else if (dir == "desc") {
+              if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                  shouldSwitch = true;
+                  break;
+              }
+          }
+      }
+      if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount++;
+      } else {
+          if (switchcount == 0 && dir == "asc") {
+              dir = "desc";
+              switching = true;
+          }
+      }
+  }
+
+  const headers = table.getElementsByTagName("th");
+  for (i = 0; i < headers.length; i++) {
+      headers[i].classList.remove("asc", "desc");
+  }
+  headers[n].classList.add(dir);
+}
